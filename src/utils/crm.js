d46b0763928b99567c830d6db551f22773ca7747
Luis Molina <luis_molina@ucol.mx>
2025-11-14 20:01:10 -0600

    removidos console.log en generarPdf...

--- src/utils/crm.js
+++ src/utils/crm.js
@@ -587,11 +587,8 @@ async function generarPdf(carrito_id) {
   }
   let {data} = await verCarrito(carrito_id);
   let title = 'Error al generar el PDF, el folio del carrito no existe. ';
-  console.log(1, {data});
   evalError(data, title);
-  console.log(2, {data});
   if (data.error && data.error === true) {
-  console.log(3, {data});
     return data;
   }
   
