2e78403a35624b4997bea832d2962902ff905838
Juan Pablo Sanchez Bracamontes <juanpablosanchezbracamontes@gmail.com>
2025-12-17 20:38:13 -0600

    Act, edicion para hacer correciones alos archivos con conflictos

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,17 +2,17 @@
 
 module.exports = {
   apps: [{
-    name: 'Daiko',
+    name: 'DaikoV2',
     script: 'server.js', // o el archivo principal de tu app
     // instances: 'max', // usar todos los cores disponibles
     exec_mode: 'fork', // modo cluster para mejor rendimiento
     env: {
       NODE_ENV: 'development',
-      PORT: 3030
+      PORT: 3031
     },
     env_production: {
       NODE_ENV: 'production',
-      PORT: 3030
+      PORT: 3031
     }
   }]
 };
