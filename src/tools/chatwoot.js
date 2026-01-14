1ae4ba83bebe0ebab4094ad92f34f5c5efc4ac5a
Juan Pablo Sanchez Bracamontes <juanpablosanchezbracamontes@gmail.com>
2026-01-13 18:14:50 -0600

    Act, edicion para mostrar informacion del sender para restructurar funcion resolverCanonico, Version 002

--- src/tools/chatwoot.js
+++ src/tools/chatwoot.js
@@ -61,7 +61,7 @@ function extraerDatosWebhook(webhookData) {
       let sender = webhookData.conversation.meta.sender;
       let OPENAI_APIKEY = await getOPENAI_APIKEY(webhookData.token, webhookData.account_id);
 console.log({sender: sender, webhookData});
-
+console.log({account_id: webhookData.account_id});
 
       // Extraer datos del webhook
       const extractedData = extraerDatosWebhook(webhookData);
