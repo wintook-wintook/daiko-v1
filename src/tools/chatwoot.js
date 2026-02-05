49895917a5c957300104cd83133933e4ab47ed5b
Luis Molina <luis_molina@ucol.mx>
2026-02-04 18:50:14 -0600

    fix deteccion de reinicio en historial con y sin acentos

--- src/tools/chatwoot.js
+++ src/tools/chatwoot.js
@@ -558,7 +558,7 @@ const getMessages = async (token, account_id, conversation_id, contextStr) => {
   idx = -1;
   for (var i = messages.length - 1; i >= 0; i--) {
     let message = messages[i];
-    if (message.role != 'user' && (message.content.includes('conversación nueva') || message.content.includes('nueva conversación'))) {
+    if (message.role != 'user' && (message.content.includes('conversación nueva') || message.content.includes('conversacion nueva') || message.content.includes('nueva conversación') || message.content.includes('nueva conversacion'))) {
       idx = i;
       break;
     }
