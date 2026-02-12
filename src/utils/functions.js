fb804b1a8fc2bf5b366715c30946aba1d6ae935e
Luis Molina <luis_molina@ucol.mx>
2026-02-11 18:58:25 -0600

    aumentar timeout default de axios de 10s a 30s para conexiones lentas

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -8,7 +8,7 @@ let delay = (ms) => {
 let getApiData = async (config, retryCount = 1) => {
   try {
     if (!config.timeout) {
-      config.timeout = 10000; // 10 segundos default
+      config.timeout = 30000; // 30 segundos default
     }
     let response = await axios(config);
     if (retryCount < 1) {
